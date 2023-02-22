c1314a1c32115ed872b74f5c3c616746a4a625ca
PC-imatge-2022-2023 <licenses@enti.cat>
2023-02-22 21:02:35 +0100

    creando el patrol de los enemigos con patrolpoints

--- Assets/true project/Scripts/Player/FPSController.cs
+++ Assets/true project/Scripts/Player/FPSController.cs
@@ -2,26 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class FPSController : MonoBehaviour
 {
     public Camera Cam;
 
-    public float mouseHorizontal = 2.5f;
-    public float mouseVertical = 0.3f;
+    //velocidad de movimiento de la camara
+    private float mouseHorizontal = 1f;
+    private float mouseVertical = 1f;
 
     float h_mouse;
     float v_mouse;
 
     private float speed;
+    [Header("Stats")]
     public float moveSpeed = 2.5f;
     public float silenceSpeed = 0.3f;
+    public float shootInterval;
 
     float h;
     float v;
 
+    CharacterController characterController;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        characterController = GetComponent<CharacterController>();
+
         //esconder el mouse
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -34,37 +42,46 @@ public class FPSController : MonoBehaviour
 
     void Move()
     {
+        //si el player esta tocando el suelo
+        //if (characterController.isGrounded)
+        //{
 
-        //movimiento de camara
-        h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
-        v_mouse = mouseVertical * -Input.GetAxis("Mouse Y");
+            //movimiento de camara
+            h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
+            v_mouse = mouseVertical * -Input.GetAxis("Mouse Y");
 
-        transform.Rotate(0, h_mouse, 0);
-        Cam.transform.Rotate(v_mouse, 0, 0);
+            transform.Rotate(0, h_mouse, 0);
+            Cam.transform.Rotate(v_mouse, 0, 0);
 
-        //movimiento del player
+            //movimiento del player
 
-        speed = moveSpeed;
-
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-
-        Vector3 direction = new Vector3(h, 0, v);
-
-        transform.Translate(direction * moveSpeed * Time.deltaTime);
-
-        //pa correr
-        if (Input.GetButton("Fire3"))
-        {
-            speed = silenceSpeed;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
-        else
-        {
             speed = moveSpeed;
-            transform.Translate(direction * speed * Time.deltaTime);
-        }
 
-        Vector3 floor = transform.TransformDirection(Vector3.down);
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            Vector3 direction = new Vector3(h, 0, v);
+
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+            //pa correr
+            if (Input.GetButton("Fire3"))
+            {
+                speed = silenceSpeed;
+                transform.Translate(direction * speed * Time.deltaTime);
+            }
+            else
+            {
+                speed = moveSpeed;
+                transform.Translate(direction * speed * Time.deltaTime);
+            }
+
+            Vector3 floor = transform.TransformDirection(Vector3.down);
+        //}
+    }
+
+    private void Shoot() {
+        
+        
     }
 }
--- Assets/true project/Scripts/enemy/AI_Enemy.cs
+++ Assets/true project/Scripts/enemy/AI_Enemy.cs
@@ -5,17 +5,39 @@ using UnityEngine.AI;
 
 public class AI_Enemy : MonoBehaviour
 {
-   
-    public Transform Objetivo;
-    public float Velocidad;
+    public Transform[] patrolPoints;
+    public GameObject Player;
+
     public NavMeshAgent IA;
+
+    public float Velocidad;
 
     public Animation Anim;
 
     void Update()
     {
         IA.speed = Velocidad;
-        IA.SetDestination(Objetivo.position);
+    }
+
+    private void Patrol(GameObject destination) {
+        for (int i = 0; i < patrolPoints.Length; i++)
+        {
+            destination = patrolPoints[i].gameObject;
+            IA.SetDestination(destination.transform.position);
+            if (i > patrolPoints.Length){ i = 0; }        
+        }
+    }
+
+    private void FollowPlayer() {
+        IA.SetDestination(Player.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PatrolPoint")
+        {
+            Patrol
+        }
     }
 
 }
