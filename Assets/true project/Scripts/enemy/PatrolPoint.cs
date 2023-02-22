d31c40e0522ff8ec58420534387a2758ae440e08
PC-imatge-2022-2023 <licenses@enti.cat>
2023-02-22 21:03:15 +0100

    creando el patrol de los enemigos con patrolpoints

---
+++ Assets/true project/Scripts/enemy/PatrolPoint.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPoint : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(this.transform.position, 1.0f);
+    }
+}
