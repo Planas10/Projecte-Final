d6f6381aeac9618e9fe9848fb8cc467c6a147dc1
victorfont <si>
2023-04-26 15:40:38 +0200

    Audios

---
+++ Assets/true project/Scripts/ScriptsAudio/Reload.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour
+{
+    public AudioSource audioSource; // Reference to the AudioSource component
+
+    void Start()
+    {
+        // Get the AudioSource component attached to the same GameObject
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        // Check if the left mouse button is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Play the sound
+            audioSource.Play();
+        }
+    }
+}
+
+
+
+
+
+
